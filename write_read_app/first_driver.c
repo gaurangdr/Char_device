b434b6f91d77d081f926405f4cdccef6d8ba7550
Gaurang Rathod <gaurang.rathod@softnautics.com>
2017-11-14 15:11:03 +0530

    Mutex lock included in LKM

--- write_read_app/first_driver.c
+++ write_read_app/first_driver.c
@@ -4,16 +4,15 @@
 #include<linux/kdev_t.h> /* major, minor number macro */
 #include<linux/fs.h> /* register number */
 #include<linux/init.h> /* module init, exit */
-
 #include<linux/device.h> /* device class */
 #include<linux/cdev.h> /* cdev, init, add */
+#include <linux/uaccess.h> /* copy to user */
+#include<linux/mutex.h> /* for mutex lock */
+
 #define DEVICE_NAME "MY_First_Driver"
 #define CLASS_NAME "My_Device_Class"
 
-#include <linux/uaccess.h> /* copy to user */
-
 static dev_t number;
-
 static struct class *device_class;
 static struct cdev char_device;
 
@@ -21,48 +20,11 @@ static int nopen;
 static char message[20];
 static short msg_len;
 
-/* my_file_functions */
-static int my_open(struct inode *i, struct file *f)
-{
-	nopen++;
-	printk(KERN_INFO "Driver: open %d times\n", nopen);
-	return 0;
-}
-static int my_close(struct inode *i, struct file *f)
-{
-	printk(KERN_INFO "Driver: close()\n");
-	return 0;
-}
-static ssize_t my_read(struct file *f, char __user *buffer, size_t len,loff_t *off)
-{
-	int error_count = 0;
-	error_count = copy_to_user(buffer, message, msg_len);
-	if (error_count == 0) {
-		printk(KERN_INFO "Driver: read %d char\n", msg_len);
-		msg_len = 0;
-		return 0;	
-	} else {
-		printk(KERN_INFO "Fail to read %d char\n", error_count);
-		error_count = 0;
-		return -EFAULT;
-	}
-}
-static ssize_t my_write(struct file *f, const char __user *buffer, size_t len,loff_t *off)
-{
-/*	sprintf(message, "%s", buffer);
-	msg_len = strlen(message);
-*/
-	int error_count = 0;
-	error_count = copy_from_user(message, buffer, len);
-	if (error_count == 0) {
-		msg_len = strlen(message);
-		printk(KERN_INFO "Driver: write() %lu char\n", len);
-		return len;
-	} else {
-		printk(KERN_INFO "Fail to read %d char\n", error_count);
-		return -EFAULT;
-	}
-}
+static int my_open(struct inode *i, struct file *f);
+static int my_close(struct inode *i, struct file *f);
+static ssize_t my_read(struct file *f, char __user *buffer, size_t len,loff_t *off);
+static ssize_t my_write(struct file *f, const char __user *buffer, size_t len,loff_t *off);
+
 static struct file_operations my_fop =
 {
 	.owner = THIS_MODULE,
@@ -72,6 +34,7 @@ static struct file_operations my_fop =
 	.write = my_write
 };
 
+static DEFINE_MUTEX(my_mutex_lock); /* define the mutex */
 
 /* constructor */
 static int __init my_init(void)
@@ -80,6 +43,9 @@ static int __init my_init(void)
 	struct device *dev_ret;
 
 	printk(KERN_INFO "Welcome\n");
+
+	/* mutex init */
+	mutex_init(&my_mutex_lock);
 
 	/* Major minor number */
 	ret = alloc_chrdev_region(&number, 0, 1, DEVICE_NAME);
@@ -118,6 +84,7 @@ static int __init my_init(void)
 
 static void __exit my_exit(void)
 {
+	mutex_destroy(&my_mutex_lock);
 	cdev_del(&char_device);
 	device_destroy(device_class, number);
 	class_destroy(device_class);
@@ -131,3 +98,51 @@ module_exit(my_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Gaurang Rathod <gaurang.rathod@softnautics.com>");
 MODULE_DESCRIPTION("First char driver");
+
+/* my_file_functions */
+static int my_open(struct inode *i, struct file *f)
+{
+	if (!mutex_trylock(&my_mutex_lock)) {
+		printk(KERN_ALERT "Device is used by other app\n");
+		return -EBUSY;
+	}
+	nopen++;
+	printk(KERN_INFO "Driver: open %d times\n", nopen);
+	return 0;
+}
+static int my_close(struct inode *i, struct file *f)
+{
+	printk(KERN_INFO "Driver: close()\n");
+	mutex_unlock(&my_mutex_lock);
+	return 0;
+}
+static ssize_t my_read(struct file *f, char __user *buffer, size_t len,loff_t *off)
+{
+	int error_count = 0;
+	error_count = copy_to_user(buffer, message, msg_len);
+	if (error_count == 0) {
+		printk(KERN_INFO "Driver: read %d char\n", msg_len);
+		msg_len = 0;
+		return 0;	
+	} else {
+		printk(KERN_INFO "Fail to read %d char\n", error_count);
+		error_count = 0;
+		return -EFAULT;
+	}
+}
+static ssize_t my_write(struct file *f, const char __user *buffer, size_t len,loff_t *off)
+{
+/*	sprintf(message, "%s", buffer);
+	msg_len = strlen(message);
+*/
+	int error_count = 0;
+	error_count = copy_from_user(message, buffer, len);
+	if (error_count == 0) {
+		msg_len = strlen(message);
+		printk(KERN_INFO "Driver: write() %lu char\n", len);
+		return len;
+	} else {
+		printk(KERN_INFO "Fail to read %d char\n", error_count);
+		return -EFAULT;
+	}
+}
